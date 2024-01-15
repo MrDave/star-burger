f5f22c2fcadc936505964641b1618f6843b69f51
David Schnoll <davidschnoll@gmail.com>
2024-01-15 20:05:16 +0200

    Create Location model

--- coordinatesapp/models.py
+++ coordinatesapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    address = models.TextField(verbose_name="адрес", unique=True)
+    lon = models.FloatField(verbose_name="долгота", null=True, blank=True)
+    lat = models.FloatField(verbose_name="широта", null=True, blank=True)
+    last_fetched = models.DateTimeField(verbose_name="дата проверки координат")
+
+    def __str__(self):
+        return self.address

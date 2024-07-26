eba3a2ea9fd1db1aed14ae3275d492780c7299e8
David Schnoll <davidschnoll@gmail.com>
2024-07-26 15:32:52 +0300

    Edit deploy script and conf

--- deployment/do_server/deploy_docker.sh
+++ deployment/do_server/deploy_docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker compose up -d
-mkdir -p media
-mkdir -p static
-mount --bind /var/lib/docker/volumes/do_server_static/_data/ /opt/star-burger/static/
+mkdir -p /opt/star-burger/media/
+mkdir -p /opt/star-burger/staticfiles/
+mount --bind /var/lib/docker/volumes/do_server_static/_data/ /opt/star-burger/staticfiles/
 mount --bind /var/lib/docker/volumes/do_server_media/_data/ /opt/star-burger/media/
-cp starburger_docker.conf /etc/nginx/sites-available
-ln -sf /etc/nginx/sites-available/starburger_docker.conf /etc/nginx/sites-enabled
+cp star-burger.conf /etc/nginx/sites-available
+ln -sf /etc/nginx/sites-available/star-burger.conf /etc/nginx/sites-enabled
 nginx -s reload
